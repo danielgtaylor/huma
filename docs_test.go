fd923c026d4c3368793e632845ea6db90f3f7e9b
Daniel G. Taylor <danielgtaylor@gmail.com>
2020-03-26 22:10:36 -0700

    tests: add docs tests

---
+++ docs_test.go
@@ -0,0 +1,41 @@
+package huma
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+var handlers = []struct {
+	name    string
+	handler func(*gin.Context, *OpenAPI)
+}{
+	{"RapiDoc", RapiDocHandler},
+	{"ReDoc", ReDocHandler},
+	{"SwaggerUI", SwaggerUIHandler},
+}
+
+func TestDocHandlers(outer *testing.T) {
+	for _, tt := range handlers {
+		local := tt
+		outer.Run(fmt.Sprintf("%v", tt.name), func(t *testing.T) {
+			g := gin.New()
+			r := NewRouterWithGin(g, &OpenAPI{
+				Title:   "Docs Test",
+				Version: "1.0.0",
+			})
+
+			r.SetDocsHandler(local.handler)
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/docs", nil)
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t, http.StatusOK, w.Code)
+		})
+	}
+}
