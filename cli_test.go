aa079f84367fee838d78cdae7e4d9a8d8f8e6728
Daniel G. Taylor <danielgtaylor@gmail.com>
2020-04-04 16:15:24 -0700

    test: add cli tests

---
+++ cli_test.go
@@ -0,0 +1,21 @@
+package huma
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestServerShutdown(t *testing.T) {
+	r := NewTestRouter(t)
+
+	go func() {
+		r.Root().Run(nil, []string{})
+	}()
+
+	time.Sleep(1 * time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	r.Shutdown(ctx)
+}
