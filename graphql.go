83d0bb89672aa90d985b82c85888052fe93dd38c
Jeff Kalbacher <jkalbacher@istreamplanet.com>
2022-09-12 10:39:41 -0700

    fix: use camel casing on last path param to adhere to graphQL regex

--- graphql.go
+++ graphql.go
@@ -172,6 +172,8 @@ func (r *Router) handleOperation(config *GraphQLConfig, parentName string, field
 	// `/things/{thing-id}` -> `thingsItem(thingId)`
 	// `/things/{thing-id}/sub` -> `sub(thingId)`
 	parts := strings.Split(strings.Trim(resource.path, "/"), "/")
+	// v2, {org}, channels, {channel-id}, preview-streams
+	// last = preview-streams
 	last := parts[len(parts)-1]
 	for i := len(parts) - 1; i >= 0; i-- {
 		if parts[i][0] == '{' {
@@ -182,6 +184,7 @@ func (r *Router) handleOperation(config *GraphQLConfig, parentName string, field
 		}
 		break
 	}
+	last = casing.LowerCamel(last)
 
 	// Setup input arguments (i.e. OpenAPI operation params).
 	args := graphql.FieldConfigArgument{}
