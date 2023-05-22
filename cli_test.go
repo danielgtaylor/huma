3ddb69247df439dbbd20b4108e4aecc2f62d4ef8
Daniel G. Taylor <danielgtaylor@gmail.com>
2023-05-22 10:03:23 -0700

    test: more CLI test coverage

--- cli_test.go
+++ cli_test.go
@@ -2,7 +2,9 @@ package huma
 
 import (
 	"bytes"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
@@ -28,11 +30,16 @@ func TestCLIPlain(t *testing.T) {
 	cli.Run()
 }
 
-func TestCLITags(t *testing.T) {
+func TestCLIAdvanced(t *testing.T) {
+	type DebugOption struct {
+		Debug bool `doc:"Enable debug mode." default:"false"`
+	}
+
 	type Options struct {
-		Debug bool   `doc:"Enable debug mode." default:"false"`
-		Host  string `doc:"Hostname to listen on."`
-		Port  int    `doc:"Port to listen on." short:"p" default:"8000"`
+		// Example of option composition via embedded type.
+		DebugOption
+		Host string `doc:"Hostname to listen on."`
+		Port int    `doc:"Port to listen on." short:"p" default:"8000"`
 	}
 
 	cli := NewCLI(func(cli CLI, options *Options) {
@@ -44,8 +51,15 @@ func TestCLITags(t *testing.T) {
 		})
 	})
 
+	// A custom pre-run isn't overwritten and should still work!
+	customPreRun := false
+	cli.Root().PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		customPreRun = true
+	}
+
 	cli.Root().SetArgs([]string{"--debug", "--host", "localhost", "--port", "8001"})
 	cli.Run()
+	assert.True(t, customPreRun)
 }
 
 func TestCLIHelp(t *testing.T) {
@@ -91,4 +105,57 @@ func TestCLICommandWithOptions(t *testing.T) {
 	cli.Run()
 
 	assert.True(t, wasSet)
+}
+
+func TestCLIShutdown(t *testing.T) {
+	type Options struct{}
+
+	started := false
+	stopping := make(chan bool, 1)
+	cli := NewCLI(func(cli CLI, options *Options) {
+		cli.OnStart(func() {
+			started = true
+			<-stopping
+		})
+		cli.OnStop(func() {
+			stopping <- true
+		})
+	})
+
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}()
+
+	cli.Root().SetArgs([]string{})
+	cli.Run()
+	assert.True(t, started)
+}
+
+func TestCLIBadType(t *testing.T) {
+	type Options struct {
+		Debug []struct{}
+	}
+
+	assert.Panics(t, func() {
+		NewCLI(func(cli CLI, options *Options) {})
+	})
+}
+
+func TestCLIBadDefaults(t *testing.T) {
+	type OptionsBool struct {
+		Debug bool `default:"notabool"`
+	}
+
+	type OptionsInt struct {
+		Debug int `default:"notanint"`
+	}
+
+	assert.Panics(t, func() {
+		NewCLI(func(cli CLI, options *OptionsBool) {})
+	})
+
+	assert.Panics(t, func() {
+		NewCLI(func(cli CLI, options *OptionsInt) {})
+	})
 }
