a9366cbf59eb63b8f24a2c01aef59fbc08bab5a7
Daniel G. Taylor <danielgtaylor@gmail.com>
2024-01-31 16:40:37 -0800

    feat: embed UUID validation, remove useless idn-hostname validator

--- validate.go
+++ validate.go
@@ -1,6 +1,7 @@
 package huma
 
 import (
+	"errors"
 	"fmt"
 	"math"
 	"net"
@@ -9,11 +10,9 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 	"unsafe"
-
-	"github.com/google/uuid"
-	"golang.org/x/net/idna"
 )
 
 // ValidateMode describes the direction of validation (server -> client or
@@ -217,10 +216,7 @@ func validateFormat(path *PathBuffer, str string, s *Schema, res *ValidateResult
 		if !(rxHostname.MatchString(str) && len(str) < 256) {
 			res.Add(path, str, "expected string to be RFC 5890 hostname")
 		}
-	case "idn-hostname":
-		if _, err := idna.ToASCII(str); err != nil {
-			res.Addf(path, str, "expected string to be RFC 5890 hostname: %v", err)
-		}
+	// TODO: proper idn-hostname support... need to figure out how.
 	case "ipv4":
 		if ip := net.ParseIP(str); ip == nil || ip.To4() == nil {
 			res.Add(path, str, "expected string to be RFC 2673 ipv4")
@@ -235,7 +231,7 @@ func validateFormat(path *PathBuffer, str string, s *Schema, res *ValidateResult
 		}
 		// TODO: check if it's actually a reference?
 	case "uuid":
-		if _, err := uuid.Parse(str); err != nil {
+		if err := validateUUID(str); err != nil {
 			res.Addf(path, str, "expected string to be RFC 4122 uuid: %v", err)
 		}
 	case "uri-template":
@@ -740,5 +736,98 @@ func (v *ModelValidator) Validate(typ reflect.Type, value any) []error {
 	if len(v.result.Errors) > 0 {
 		return v.result.Errors
 	}
+	return nil
+}
+
+// The following is borrowed from the Google UUID package:
+// https://github.com/google/uuid/blob/v1.6.0/uuid.go
+// Copyright (c) 2009,2014 Google Inc. All rights reserved.
+// BSD licensed.
+
+type invalidLengthError struct{ len int }
+
+func (err invalidLengthError) Error() string {
+	return fmt.Sprintf("invalid UUID length: %d", err.len)
+}
+
+// xvalues returns the value of a byte as a hexadecimal digit or 255.
+var xvalues = [256]byte{
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 255, 255, 255, 255, 255, 255,
+	255, 10, 11, 12, 13, 14, 15, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 10, 11, 12, 13, 14, 15, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+	255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+}
+
+// xtob converts hex characters x1 and x2 into a byte.
+func xtob(x1, x2 byte) (byte, bool) {
+	b1 := xvalues[x1]
+	b2 := xvalues[x2]
+	return (b1 << 4) | b2, b1 != 255 && b2 != 255
+}
+
+// validateUUID returns an error if s is not a properly formatted UUID in one of the following formats:
+//
+//	xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+//	urn:uuid:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+//	xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//	{xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx}
+//
+// It returns an error if the format is invalid, otherwise nil.
+func validateUUID(s string) error {
+	switch len(s) {
+	// Standard UUID format
+	case 36:
+
+	// UUID with "urn:uuid:" prefix
+	case 36 + 9:
+		if !strings.EqualFold(s[:9], "urn:uuid:") {
+			return fmt.Errorf("invalid urn prefix: %q", s[:9])
+		}
+		s = s[9:]
+
+	// UUID enclosed in braces
+	case 36 + 2:
+		if s[0] != '{' || s[len(s)-1] != '}' {
+			return fmt.Errorf("invalid bracketed UUID format")
+		}
+		s = s[1 : len(s)-1]
+
+	// UUID without hyphens
+	case 32:
+		for i := 0; i < len(s); i += 2 {
+			_, ok := xtob(s[i], s[i+1])
+			if !ok {
+				return errors.New("invalid UUID format")
+			}
+		}
+
+	default:
+		return invalidLengthError{len(s)}
+	}
+
+	// Check for standard UUID format
+	if len(s) == 36 {
+		if s[8] != '-' || s[13] != '-' || s[18] != '-' || s[23] != '-' {
+			return errors.New("invalid UUID format")
+		}
+		for _, x := range []int{0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34} {
+			if _, ok := xtob(s[x], s[x+1]); !ok {
+				return errors.New("invalid UUID format")
+			}
+		}
+	}
+
 	return nil
 }
