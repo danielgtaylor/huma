d764e72d1dde2f78cba5c7763b358634dd45010b
Quentin Burgess <qutn.burgess@gmail.com>
2024-03-13 12:10:25 +0100

    feat(config): allow to specify an optional API prefix to play well with chi router mounting

--- api.go
+++ api.go
@@ -9,6 +9,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/url"
+	"path/filepath"
 	"reflect"
 	"regexp"
 	"strings"
@@ -176,6 +177,18 @@ type Config struct {
 
 	// Transformers are a way to modify a response body before it is serialized.
 	Transformers []Transformer
+
+	// APIPrefix allow to specify an optional, already loaded, URI prefix,
+	// thus fixing the doc rendering.
+	//
+	// rootMx, apiMx := chi.NewMux(), chi.NewMux()
+	// rootMx.Mount("/api", apiMx)
+	// cfg := huma.DefaultConfig("My API", "1.0.0")
+	// cfg.APIPrefix = "/api"
+	// api := humachi.New(apiMx, cfg)
+	// huma.AutoRegister(api, struct{})
+	// srv := http.Server{Handler: rootMx}
+	APIPrefix string
 }
 
 // API represents a Huma API wrapping a specific router.
@@ -402,7 +415,7 @@ func NewAPI(config Config, a Adapter) API {
   <body style="height: 100vh;">
 
     <elements-api
-      apiDescriptionUrl="` + config.OpenAPIPath + `.yaml"
+      apiDescriptionUrl="` + filepath.Join(config.APIPrefix, config.OpenAPIPath) + `.yaml"
       router="hash"
       layout="sidebar"
       tryItCredentialsPolicy="same-origin"
@@ -422,7 +435,7 @@ func NewAPI(config Config, a Adapter) API {
 			schema := strings.TrimSuffix(ctx.Param("schema"), ".json")
 			ctx.SetHeader("Content-Type", "application/json")
 			b, _ := json.Marshal(config.OpenAPI.Components.Schemas.Map()[schema])
-			b = rxSchema.ReplaceAll(b, []byte(config.SchemasPath+`/$1.json`))
+			b = rxSchema.ReplaceAll(b, []byte(filepath.Join(config.APIPrefix, config.SchemasPath)+`/$1.json`))
 			ctx.BodyWriter().Write(b)
 		})
 	}
