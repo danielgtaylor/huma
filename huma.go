1182cc1f916b614f0ba812dc477df56f50a93e0d
Daniel Taylor <danielgtaylor@gmail.com>
2024-12-22 09:39:20 -0800

    fix: potential body error race

--- huma.go
+++ huma.go
@@ -1336,7 +1336,7 @@ func Register[I, O any](api API, op Operation, handler func(context.Context, *I)
 							res.Errors = append(res.Errors, &ErrorDetail{
 								Location: "body",
 								Message:  err.Error(),
-								Value:    body,
+								Value:    string(body),
 							})
 							parseErrCount++
 						} else {
